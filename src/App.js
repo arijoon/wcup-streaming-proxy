ca02e2c5a09b6b78725ade5395c3ea00f2222603
Arman Yaraee <arman@yaraee.net>
2018-06-18 18:09:19 +0100

    Added both media servers

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 import Player from './Player';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
+
     this.state = {
       hideHeader: false,
       urls: [
@@ -17,26 +18,51 @@ class App extends Component {
           name: 'TSN',
           url: 's21.m3u8'
         }
-      ]
+      ],
+      activeUrl: 0,
+      servers: [
+        {
+          name: 'Media 1',
+          url: 'media1'
+        },
+        {
+          name: 'Media 2',
+          url: 'media2'
+        }
+      ],
+      activeServer: 0
     }
-
-    this.state.activeUrl = 0;
   }
 
   changePlayer(index) {
     this.setState({ activeUrl: index });
   }
 
+  changeServer(index) {
+    this.setState({ activeServer: index });
+  }
+
+
   getLink(media, item) {
     return `/${media}/${item}`;
   }
 
   render() {
-    const { urls, activeUrl, hideHeader } = this.state;
+    const { urls, activeUrl, servers, activeServer, hideHeader } = this.state;
     return (
       <div className="App">
         {!hideHeader && this.renderHeader()}
         <div className="container">
+          <div className="row">
+            <div className="input-group mb-3">
+              <div className="input-group-append center">
+                {servers.map((server, index) =>
+                  <button key={server.url} className={`btn btn-outline-secondary ${index === activeServer && 'active'}`} type="button"
+                    onClick={() => this.changeServer(index)}>{server.name}</button>
+                )}
+              </div>
+            </div>
+          </div>
           <div className="row">
             <div className="input-group mb-3">
               <div className="input-group-append center">
@@ -49,7 +75,7 @@ class App extends Component {
           </div>
         </div>
         <div className="App-intro">
-          <Player source={this.getLink('media1', urls[activeUrl].url)} />
+          <Player source={this.getLink(servers[activeServer].url, urls[activeUrl].url)} />
         </div>
       </div>
     );
